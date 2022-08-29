e344b318c2b689cdff60091182f011425f90b919
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-08-29 17:46:47 +0300

    Initial commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
+import Cocktail from "./Cocktail";
+
+const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">no cocktails matched your search</h2>;
+  }
+
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktail list component</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;
---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        imperdiet malesuada maximus. Phasellus tempor, erat quis eleifend
+        gravida, risus arcu varius purus, eu viverra eros libero in nibh. Donec
+        iaculis auctor ligula ut placerat. Nunc nulla justo, fermentum vitae
+        consequat at, fermentum a orci. Nulla at turpis id purus dapibus tempus
+        sed et ex. Nullam consequat quis odio in scelerisque. Vestibulum sit
+        amet pellentesque justo. Nullam non elementum lorem. Fusce gravida
+        vehicula lectus quis mollis. Nunc finibus dictum lectus eu varius. Sed
+        efficitur volutpat vestibulum. Ut aliquet ante at porttitor venenatis.
+        Nam non cursus libero, sit amet tempus odio. Duis mattis fermentum magna
+        eget congue.
+      </p>
+    </section>
+  );
+};
+
+export default About;
