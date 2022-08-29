0c670ff6feac8748f9c449f6f8ca2996cc518744
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-08-29 18:04:15 +0300

    Updates

---
+++ precache-manifest.169f6e5236779954f8ce9400ac8ae56a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2784e190f2b32f7ebf0e16e20b4e482",
+    "url": "/CocktailsBD/index.html"
+  },
+  {
+    "revision": "4ea63efb4f1ed14c2a6a",
+    "url": "/CocktailsBD/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "797050a0fe228514c9f2",
+    "url": "/CocktailsBD/static/js/2.39eed288.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/CocktailsBD/static/js/2.39eed288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea63efb4f1ed14c2a6a",
+    "url": "/CocktailsBD/static/js/main.947fd282.chunk.js"
+  },
+  {
+    "revision": "59ad476f80acf7106c63",
+    "url": "/CocktailsBD/static/js/runtime-main.ad965c05.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/CocktailsBD/static/media/logo.f3ad7664.svg"
+  }
+]);
